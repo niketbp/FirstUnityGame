c6b62414391204fdcde755ae53ddef73fcb3b718
Niket Parikh <niketbp@umich.edu>
2017-01-06 12:43:12 -0500

    Have a working game! Still have quite a bit to add though

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float force;
+	bool gameStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space) && !gameStarted) {
+			transform.SetParent (null);
+			rb.isKinematic = false;
+			rb.AddForce (new Vector2 (force, force));
+			gameStarted = true;
+		}
+	}
+}
