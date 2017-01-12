092eeb5e6bc43745db73922ed618bb0d3da2a6db
Niket Parikh <niketbp@umich.edu>
2017-01-12 14:09:24 -0500

    Added script for collision with bottom wall, will be used for decrementing lives

---
+++ Assets/Scripts/BottomWallScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomWallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.gameObject.tag == "Ball") {
+//			Application.Quit();
+			print ("reached");
+		}
+	}
+}
