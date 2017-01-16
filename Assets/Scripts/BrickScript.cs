aa1c74fe505632fb87f969f612f08c75ae4e4104
Niket Parikh <niketbp@umich.edu>
2017-01-16 17:02:29 -0500

    Added score

--- Assets/Scripts/BrickScript.cs
+++ Assets/Scripts/BrickScript.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class BrickScript : MonoBehaviour {
 
+	public UIManagerScript ui;
+
 	// Use this for initialization
 	void Start () {
-		
+		ui = GameObject.FindWithTag ("UI").GetComponent<UIManagerScript> ();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +20,6 @@ public class BrickScript : MonoBehaviour {
 		if (collision.gameObject.tag == "Ball") {
 			Destroy (gameObject);
 		}
+		ui.incrementScore ();
 	}
 }
---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerScript : MonoBehaviour {
+
+	int score = 0;
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void incrementScore(){
+		score++;
+		scoreText.text = "Score: " + score;
+	}
+}
